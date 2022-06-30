5df6ea5f9327724e6501c4cf331cd3ef5d5215a9
pervanne69 <samvel.pogosyan2006@gmail.com>
2022-07-01 01:34:16 +0300

    sixth commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
+
 const body = document.body
+const resourcesPlaning = body.querySelector('.resources__planing')
 
 body.querySelectorAll('.questions__question').forEach(el => {
     const questionText = el.querySelector('.question__text')
@@ -6,4 +8,54 @@ body.querySelectorAll('.questions__question').forEach(el => {
         el.classList.toggle('active')
         el.classList.toggle('passive')
     })
+})
+
+resourcesPlaning.addEventListener('click', e => {
+    if (e.target === e.currentTarget) {
+        const planingCardActive = body.querySelector('.planing__card-active')
+        const cycle = body.querySelector('.resources__cycle-active')
+
+        planingCardActive.classList.toggle('planing__card-active')
+        planingCardActive.classList.toggle('planing__card-passive')
+
+        cycle.classList.toggle('resources__cycle-active')
+        cycle.classList.toggle('resources__cycle-passive')
+
+        if (e.offsetX > 100) {
+            if (planingCardActive.id === "planing__card-3") {
+                const planingCardActiveFirstChild = body.querySelector('#planing__card-1')
+                const resourcesCycleFirstChild = body.querySelector('#resources__cycle-1')
+
+                planingCardActiveFirstChild.classList.toggle('planing__card-active')
+                planingCardActiveFirstChild.classList.toggle('planing__card-passive')
+
+                resourcesCycleFirstChild.classList.toggle('resources__cycle-active')
+                resourcesCycleFirstChild.classList.toggle('resources__cycle-passive')
+            } else {
+                planingCardActive.nextElementSibling.classList.toggle('planing__card-active')
+                planingCardActive.nextElementSibling.classList.toggle('planing__card-passive')
+
+                cycle.nextElementSibling.classList.toggle('resources__cycle-active')
+                cycle.nextElementSibling.classList.toggle('resources__cycle-passive')
+            }
+        } else {
+            if (planingCardActive.id === "planing__card-1") {
+                const planingCardActiveLastChild = body.querySelector('#planing__card-3')
+                const resourcesCycleActiveLastChild = body.querySelector('#resources__cycle-3')
+
+                planingCardActiveLastChild.classList.toggle('planing__card-active')
+                planingCardActiveLastChild.classList.toggle('planing__card-passive')
+
+                resourcesCycleActiveLastChild.classList.toggle('resources__cycle-active')
+                resourcesCycleActiveLastChild.classList.toggle('resources__cycle-passive')
+
+            } else {
+                planingCardActive.previousElementSibling.classList.toggle('planing__card-active')
+                planingCardActive.previousElementSibling.classList.toggle('planing__card-passive')
+
+                cycle.previousElementSibling.classList.toggle('resources__cycle-active')
+                cycle.previousElementSibling.classList.toggle('resources__cycle-passive')
+            }
+        }
+    }
 })
